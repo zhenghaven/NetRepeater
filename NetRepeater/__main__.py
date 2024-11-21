12b2784b0302c1b6f85fabef5529d3b8f01adf34
Haofan Zheng <hzheng6@ucsc.edu>
2024-11-20 21:30:13 -0800

    Updated main and removed old files

--- NetRepeater/__main__.py
+++ NetRepeater/__main__.py
@@ -9,81 +9,57 @@
 
 
 import argparse
-import json
-import logging
 
-from typing import Any, Dict
-
-from .Inbound import FindInboundServerCreator
-from .Outbound import FindOutboundConnector
-from . import _Meta
-from . import AddressAdder
-from . import ServerCluster
+from ModularDNS.Service import Resolver
 
 
-RemoteHostConfig = Dict[str, Any]
-LocalProtoToRemoteHostConfig = Dict[str, RemoteHostConfig]
-LocalPortToRemoteHostConfig = Dict[int, LocalProtoToRemoteHostConfig]
-LocalHostToRemoteHostConfig = Dict[str, LocalPortToRemoteHostConfig]
+def GetPackageInfo() -> dict:
+	import os
+
+	thisDir = os.path.dirname(os.path.abspath(__file__))
+	possibleRepoDir = os.path.dirname(thisDir)
+	possibleTomlPath = os.path.join(possibleRepoDir, 'pyproject.toml')
+
+	pkgInfo = {
+		'name': __package__ or __name__,
+	}
+
+	if os.path.exists(possibleTomlPath):
+		import tomllib
+		with open(possibleTomlPath, 'rb') as file:
+			tomlData = tomllib.load(file)
+		if (
+			('project' in tomlData) and
+			('name' in tomlData['project']) and
+			(tomlData['project']['name'] == pkgInfo['name'])
+		):
+			pkgInfo['description'] = tomlData['project']['description']
+			pkgInfo['version'] = tomlData['project']['version']
+			return pkgInfo
+
+	import importlib
+	pkgInfo['version'] = importlib.metadata.version(pkgInfo['name'])
+	pkgInfo['description'] = importlib.metadata.metadata(pkgInfo['name'])['Summary']
+	return pkgInfo
 
 
 def main() -> int:
-	parser = argparse.ArgumentParser(description='Network Forwarder')
+	pkgInfo = GetPackageInfo()
+
+	parser = argparse.ArgumentParser(
+		description=pkgInfo['description'],
+	)
 	parser.add_argument(
-		'--version', action='version',
-		version=f'{_Meta.__version__}'
+		'--version',
+		action='version', version=pkgInfo['version'],
 	)
 	parser.add_argument(
 		'--config', '-c', type=str, required=True,
 		help='Path to the configuration file'
 	)
-	parser.add_argument(
-		'--verbose', action='store_true',
-		help='Enable verbose logging'
-	)
 	args = parser.parse_args()
 
-	with open(args.config, 'r') as f:
-		config = json.load(f)
-
-	logFile = None
-	if 'logFile' in config:
-		logFile = config['logFile']
-
-	logLevel = logging.DEBUG if args.verbose else logging.INFO
-	logFormat = '[%(asctime)s]%(levelname)s[%(name)s] - %(message)s'
-
-	logging.basicConfig(
-		level=logLevel,
-		format=logFormat,
-		filename=logFile
-	)
-
-	# Add addresses to the devices first if specified
-	# otherwise, the servers below will fail to bind to the addresses
-	if 'addAddress' in config:
-		for dev, addrs in config['addAddress'].items():
-			addrAdder = AddressAdder.AddressAdder(dev)
-			addrAdder.AddAddresses(addrs)
-
-	svrCluster = ServerCluster.ServerCluster()
-
-	forwarders: LocalHostToRemoteHostConfig = config['forwarders']
-	for locHost, locPort2RetHost in forwarders.items():
-		for locPort, locProto2RetHost in locPort2RetHost.items():
-			locPort = int(locPort)
-			for locProto, retHost in locProto2RetHost.items():
-				inboundServerCreator = FindInboundServerCreator(locProto)
-				outboundConnector = FindOutboundConnector(retHost['proto'])
-				svrCluster.AddServer(
-					inboundServerCreator(
-						locHost, locPort, outboundConnector(**retHost)
-					)
-				)
-
-	svrCluster.ServeUntilSignals()
-
-	return 0
+	Resolver.Start(configPath=args.config)
 
 
 if __name__ == '__main__':
